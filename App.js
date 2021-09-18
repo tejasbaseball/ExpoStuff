97f381933447c3f6402c19b40a22245b8bddb3ea
tejasbaseball <55719023+tejasbaseball@users.noreply.github.com>
2021-09-17 19:10:05 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { View } from 'react-native';
+import Appbar from './components/appbar';
+import Search from './components/search';
+
+export default function App() {
+  return (
+    <View>
+      <Appbar/>
+      <Search/>
+    </View>
+  );
+}
+
+
